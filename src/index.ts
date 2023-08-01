a02a2b8a0116276b9d25105a7a1f5ffecbc76f0e
irakevine <ikevine2016@gmail.com>
2023-08-01 23:57:03 +0200

    modifying

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export const server = new ApolloServer({
     schema,
 });
 
-const port = 6000;
+const port = 3000;
 // 2
 server.listen({port}).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
